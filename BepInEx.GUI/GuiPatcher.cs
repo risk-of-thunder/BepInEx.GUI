d0a29d52a4437f68c557b6dc72efbe8708159c9a
HarbingerOfMe <harbingerofme@gmail.com>
2020-12-28 00:33:23 +0100

    Add PoC

---
+++ BepInEx.GUI/GuiPatcher.cs
@@ -0,0 +1,21 @@
+﻿using Mono.Cecil;
+using System.Collections.Generic;
+using System.Reflection;
+using System.IO;
+using System.Linq;
+using BepInEx;
+
+namespace BepInEx.GUI
+{
+    public static class GuiPatcher
+    {
+        public static IEnumerable<string> TargetDLLs => Enumerable.Empty<string>();
+        public static void Patch(AssemblyDefinition _) { }
+
+        public static void Initialize()
+        {
+            var executable = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "GraphicalUserInterface.exe");
+            System.Diagnostics.Process.Start(executable, "\"BepInEx - Risk of Rain 2\"");
+        }
+    }
+}
---
+++ GraphicalUserInterface/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace BepInEx.GUI
+{
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        public static void Main(params string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Form1 form = new Form1();
+            form.Text = string.Join(" ", args);
+            Application.Run(form);
+        }
+    }
+}
