f29627e710e5372d43b8bc77fa27fea95bb4d5e9
xiaoxiao921 <xiaoxiao921@hotmail.fr>
2021-12-22 16:26:56 +0100

    patcher cleanup

---
+++ BepInEx.GUI.Patcher/CloseGuiOnChainloaderDone.cs
@@ -0,0 +1,38 @@
+﻿using BepInEx.Logging;
+using System;
+
+namespace BepInEx.GUI.Patcher
+{
+    public class CloseGuiOnChainloaderDone : ILogListener
+    {
+        public void Dispose() { }
+
+        public void LogEvent(object sender, LogEventArgs eventArgs)
+        {
+            if (eventArgs.Data.ToString() == "Chainloader startup complete" && eventArgs.Level.Equals(LogLevel.Message))
+            {
+                Patcher.LogSource.LogMessage("Closing BepInEx.GUI");
+
+                Exit();
+            }
+        }
+
+        private void Exit()
+        {
+            try
+            {
+                Patcher.GuiProcess.Kill();
+            }
+            catch (Exception e)
+            {
+                Patcher.LogSource.LogError(e.Message);
+                Patcher.LogSource.LogError(e.StackTrace);
+            }
+            finally
+            {
+                Logger.Listeners.Remove(this);
+                Patcher.LogSource.Dispose();
+            }
+        }
+    }
+}
