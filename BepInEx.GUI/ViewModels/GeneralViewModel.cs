0c4f7a33209989c8b322ce81cab838972ec6715b
xiaoxiao921 <xiaoxiao921@hotmail.fr>
2021-12-22 19:04:04 +0100

    3 buttons from general tab works

--- BepInEx.GUI/ViewModels/GeneralViewModel.cs
+++ BepInEx.GUI/ViewModels/GeneralViewModel.cs
@@ -1,18 +1,32 @@
 using BepInEx.GUI.Models;
+using ReactiveUI;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Input;
 
 namespace BepInEx.GUI.ViewModels
 {
     public class GeneralViewModel : ViewModelBase
     {
+        public PathsInfo PathsInfo { get; }
+
         public string TargetIsLoadingCanCloseWindow { get; }
 
         public string LoadedModCountText { get; }
         public ObservableCollection<Mod> Mods { get; }
 
-        public GeneralViewModel(PathsInfo pathsInfo)
+        public PlatformInfo PlatformInfo { get; }
+
+        public ICommand OnClickOpenGameFolderCommand { get; private set; }
+        public ICommand OnClickShowLogFolderCommand { get; private set; }
+        public ICommand OnClickShowBepInExFolderCommand { get; private set; }
+
+        public GeneralViewModel(PathsInfo pathsInfo, PlatformInfo platformInfo)
         {
+            PathsInfo = pathsInfo;
+
             TargetIsLoadingCanCloseWindow = $"{pathsInfo.ProcessName} is loading, you can safely close this window.";
 
             var mods = new List<Mod>();
@@ -21,6 +35,52 @@ namespace BepInEx.GUI.ViewModels
 
             LoadedModCountText = $"Loaded Mods: {mods.Count}";
             Mods = new ObservableCollection<Mod>(mods);
+
+            PlatformInfo = platformInfo;
+
+            SetButtonCommands();
+        }
+
+        private void SetButtonCommands()
+        {
+            OnClickOpenGameFolderCommand = ReactiveCommand.Create(OnClickOpenGameFolder);
+            OnClickShowLogFolderCommand = ReactiveCommand.Create(OnClickShowLogFolder);
+            OnClickShowBepInExFolderCommand = ReactiveCommand.Create(OnClickShowBepInExFolder);
+        }
+
+        public void OnClickOpenGameFolder()
+        {
+            OpenFolder(PathsInfo.GameFolderPath);
+        }
+
+        public void OnClickShowLogFolder()
+        {
+            OpenFolder(PathsInfo.BepInExFolderPath);
+        }
+
+        public void OnClickShowBepInExFolder()
+        {
+            OpenFolder(PathsInfo.BepInExFolderPath);
+        }
+
+        private void OpenFolder(string folderPath)
+        {
+            if (Directory.Exists(folderPath))
+            {
+                var processStartInfo = new ProcessStartInfo();
+                processStartInfo.Arguments = folderPath;
+
+                if (PlatformInfo.IsWindows)
+                {
+                    processStartInfo.FileName = "explorer.exe";
+                }
+
+                Process.Start(processStartInfo);
+            }
+            else
+            {
+                Debug.Message($"{folderPath} Directory does not exist!");
+            }
         }
     }
 }
