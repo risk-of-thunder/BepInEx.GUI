ac3f52d17382ff54aaafec16e3a3d9c21c77e0d0
xiaoxiao921 <xiaoxiao921@hotmail.fr>
2021-12-22 16:49:44 +0100

    simplify PathsInfo construct

--- BepInEx.GUI/Models/PathsInfo.cs
+++ BepInEx.GUI/Models/PathsInfo.cs
@@ -2,10 +2,34 @@
 {
     public class PathsInfo
     {
-        public string BepInExVersion { get; internal set; }
+        public string? BepInExVersion { get; }
 
-        public string ProcessName { get; internal set; }
+        public string ProcessName { get; }
 
-        public string ConfigFolderPath { get; internal set; }
+        public string ConfigFolderPath { get; }
+
+        public PathsInfo(string[] args)
+        {
+            args = DefaultIfNoneProvided(args);
+
+            BepInExVersion = args[0];
+            ProcessName = args[1];
+            ConfigFolderPath = args[2];
+        }
+
+        private static string[] DefaultIfNoneProvided(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                args = new string[]
+                {
+                            "Unknown Version",
+                            "Unknown Target",
+                            string.Empty
+                };
+            }
+
+            return args;
+        }
     }
 }
