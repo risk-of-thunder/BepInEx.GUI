ff0d9d43dde8382918ab6edd8d9831f30f068286
xiaoxiao921 <xiaoxiao921@hotmail.fr>
2021-12-23 20:13:14 +0100

    Add LogEntry from WebSlog

---
+++ BepInEx.GUI/Models/LogEntry.cs
@@ -0,0 +1,47 @@
+﻿using ProtoBuf;
+using System;
+using System.IO;
+
+namespace BepInEx.GUI.Models
+{
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
+    // https://github.com/PassivePicasso/BepInEx-LogEx/tree/master/WebSlog
+    [ProtoContract]
+    public class LogEntry
+    {
+        [ProtoMember(1)]
+        public string Source { get; internal set; }
+
+        [ProtoMember(2)]
+        public string Level { get; internal set; }
+
+        [ProtoMember(3)]
+        public int LevelCode { get; internal set; }
+
+        [ProtoMember(4)]
+        public string Data { get; internal set; }
+
+        public static LogEntry? Deserialize(byte[] data)
+        {
+            if (data == null)
+            {
+                return null;
+            }
+
+            try
+            {
+                using var stream = new MemoryStream(data);
+                return Serializer.Deserialize<LogEntry>(stream);
+            }
+            catch (Exception e)
+            {
+                // todo
+            }
+
+            return null;
+        }
+    }
+
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+}
