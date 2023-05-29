180ea77f73131dc3452fd7b4b63479fdf51a695b
Quentin E. / iDeath <xiaoxiao921@hotmail.fr>
2023-05-29 20:11:54 +0200

    add icon to executable + versioning

---
+++ bepinex_gui/build.rs
@@ -0,0 +1,14 @@
+// build.rs
+
+#[cfg(windows)]
+extern crate winres;
+
+#[cfg(windows)]
+fn main() {
+    let mut res = winres::WindowsResource::new();
+    res.set_icon("assets/app_icon.ico");
+    res.compile().unwrap();
+}
+
+#[cfg(unix)]
+fn main() {}
